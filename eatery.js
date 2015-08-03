3bf95912b4418269246515de0cc813aaa1c0bf43
Ian Conery <ianconery@gmail.com>
2015-08-03 12:30:24 -0700

    showing data on page adding google maps to page

--- eatery.js
+++ eatery.js
@@ -1,11 +1,16 @@
 var app = angular.module('eatery',[]);
 
 app.controller('EateryController', function($scope, EateryFact){
-  $scope.getRandomRestaraunt = EateryFact.getRandomRestaraunt;
+  $scope.getRandomRestaraunt = function(){
+    EateryFact.getRandomRestaraunt().then(function(data){
+      $scope.data = data;
+    });
+
+  }
 
 });
 
-app.factory('EateryFact', function($http){
+app.factory('EateryFact', function($http, $location){
   var data = '?ll=37.783724,-122.408978&radius=1609&section=food&limit=5&client_id=AMPO5F53E2EG2N4FYOF3IJLUOO1JTLZ3V4G3PYOENMJIWZF4&client_secret=PWGXI0LL5TYQO2NAPPIPW2N5FVBEOU0XIAZ0WVLT04IIAP3H&v=20140806&m=foursquare'
   var getRandomRestaraunt = function () {
     console.log('I have been clicked');
@@ -17,7 +22,7 @@ app.factory('EateryFact', function($http){
       var respData = resp.data.response.groups[0].items;
       var rando = respData[Math.floor(Math.random() * respData.length)]
       console.log('here is the response', rando)
-      return resp;
+      return rando;
     });
   };
 
