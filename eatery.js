b4dd299cdcd6be7c07e211b5f5659b34f918cbbd
Ian Conery <ianconery@gmail.com>
2015-08-03 11:49:39 -0700

    Basic outline returning hardcode data from foursquare

---
+++ eatery.js
@@ -0,0 +1,27 @@
+var app = angular.module('eatery',[]);
+
+app.controller('EateryController', function($scope, EateryFact){
+  $scope.getRandomRestaraunt = EateryFact.getRandomRestaraunt;
+
+});
+
+app.factory('EateryFact', function($http){
+  var data = '?ll=37.783724,-122.408978&radius=1609&section=food&limit=5&client_id=AMPO5F53E2EG2N4FYOF3IJLUOO1JTLZ3V4G3PYOENMJIWZF4&client_secret=PWGXI0LL5TYQO2NAPPIPW2N5FVBEOU0XIAZ0WVLT04IIAP3H&v=20140806&m=foursquare'
+  var getRandomRestaraunt = function () {
+    console.log('I have been clicked');
+    return $http({
+      method: 'GET',
+      url: 'https://api.foursquare.com/v2/venues/explore' + data
+    })
+    .then(function (resp) {
+      var respData = resp.data.response.groups[0].items;
+      var rando = respData[Math.floor(Math.random() * respData.length)]
+      console.log('here is the response', rando)
+      return resp;
+    });
+  };
+
+  return {
+    getRandomRestaraunt: getRandomRestaraunt
+  }
+})
