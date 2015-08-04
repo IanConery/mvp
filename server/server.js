e0883195a5eda7341b4fe37779a5ecb5a7ed63a3
Ian Conery <ianconery@gmail.com>
2015-08-04 11:19:38 -0700

    added firebase because my computer sucks

--- server/server.js
+++ server/server.js
@@ -1,20 +1,11 @@
+var Firebase = require("firebase");
 var express = require('express');
-var mongoose = require('mongoose');
 var morgan = require('morgan');
-// var bodyParser = require('body-parser');
 
 var app = express();
-
-mongoose.connect('mongodb://127.0.0.1/what-should-i-eat'); 
-db = mongoose.connection;
-db.on('error',console.error.bind(console, 'You messed up'));
-db.once('open', function(){
-  console.log('Success!! connected to mongoose database')
-});
+var myFirebaseRef = new Firebase("https://blistering-fire-5536.firebaseio.com/");
   
 app.use(morgan('dev'));
-// app.use(bodyParser.urlencoded({extended: true}));
-// app.use(bodyParser.json());
 
 app.listen(8080);
 
